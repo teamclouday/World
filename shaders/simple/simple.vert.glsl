d9c66588547fa527e0ea38633efd2be23ed45dea
teamclouday <teamclouday@gmail.com>
2020-05-23 18:41:10 -0400

    update shaders

--- shaders/simple/simple.vert.glsl
+++ shaders/simple/simple.vert.glsl
@@ -3,11 +3,21 @@
 
 layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inColor;
+layout(location = 2) in vec2 coord;
 
 layout(location = 0) out vec3 fragColor;
+layout(location = 1) out vec2 fragCoord;
+
+layout(binding = 0) uniform CameraUniform
+{
+	mat4 model;
+	mat4 view;
+	mat4 proj;
+} ubo;
 
 void main()
 {
-	gl_Position = vec4(inPosition, 1.0);
+	gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 1.0);
 	fragColor = inColor;
+	fragCoord = coord;
 }
